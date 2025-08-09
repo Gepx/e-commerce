45ac2126e1c7f7237872842d3a500740d7ab0d65
egipsinargo <egipsinargo123@gmail.com>
2025-08-09 18:08:26 +0700

    feat: add route to auth page

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,7 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from './layouts/RootLayout';
 import Home from './pages/Home';
+import Auth from './pages/Auth';
+import AuthLayout from './layouts/AuthLayout';
+import Register from './components/auth/Register';
 
 export const router = createBrowserRouter([
-  { path: '/', element: <RootLayout />, children: [{ index: true, element: <Home /> }] }
+  { path: '/', element: <RootLayout />, children: [{ index: true, element: <Home /> }] },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      { index: true, element: <Auth /> },
+      { path: 'register', element: <Register /> }
+    ]
+  }
 ]);
