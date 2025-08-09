be0c783d41b9f9679fb5f2a5d3bdcd5d9d0fc068
egipsinargo <egipsinargo123@gmail.com>
2025-08-09 17:55:34 +0700

    feat(auth): setup routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/authController");
+const OAuthController = require("../controllers/oauthController");
+const { validateSchema } = require("../utils/validateSchema");
+const { registerSchema, loginSchema } = require("../schemas/authSchema");
 
-router.post("/register", AuthController.register);
-router.post("/login", AuthController.login);
+router.post(
+  "/register",
+  validateSchema(registerSchema),
+  AuthController.register
+);
+router.post("/login", validateSchema(loginSchema), AuthController.login);
+router.post("/logout", AuthController.logout);
+
+// Provider Auth Routes
+router.get("/:provider", OAuthController.redirect);
+router.get("/:provider/callback", OAuthController.callback);
 
 module.exports = router;
