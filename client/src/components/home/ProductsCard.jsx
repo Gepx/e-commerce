48998ca153eb8dd1ba77cf983aaf84e8858ca3b3
egipsinargo <egipsinargo123@gmail.com>
2025-08-14 22:28:15 +0700

    feat: add product card (static)

--- client/src/components/home/ProductsCard.jsx
+++ client/src/components/home/ProductsCard.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import data from '@/json/productDummy.json';
 import { Card, CardContent, CardDescription, CardFooter, CardTitle } from '../ui/card';
 import { Checkbox } from '../ui/checkbox';
 import { Input } from '../ui/input';
 import { FilterIcon } from 'lucide-react';
 import { Button } from '../ui/button';
+import PaginationComponent from '../pagination/Pagination';
+import { useNavigate } from 'react-router-dom';
 
 const ProductsCard = () => {
+  const pageSize = 15;
   const products = data.products;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const navigate = useNavigate();
+
+  const totalPages = Math.ceil(data.products.length / pageSize);
+
+  const paginatedProducts = useMemo(() => {
+    const startPage = (currentPage - 1) * pageSize;
+    return products.slice(startPage, startPage + pageSize);
+  }, [products, currentPage]);
+
   const uniqueCategories = [...new Set(products.map((product) => product.productCategory))];
   const categories = uniqueCategories.map((category) => {
     const count = products.filter((p) => p.productCategory === category).length;
@@ -59,10 +73,11 @@ const ProductsCard = () => {
         </CardContent>
       </Card>
       <div className="col-span-4 grid grid-cols-2 lg:grid-cols-5 gap-4">
-        {products.map((product) => (
+        {paginatedProducts.map((product) => (
           <Card
             key={product.productId}
-            className="w-fit overflow-hidden p-0 border-none gap-0 cursor-pointer flex flex-col">
+            className="w-fit h-[284px] overflow-hidden p-0 border-none gap-0 cursor-pointer flex flex-col"
+            onClick={() => navigate(`/product/${product.productId}`)}>
             <div className="w-[180px] h-[180px] overflow-hidden">
               <img
                 src="https://miro.medium.com/v2/resize:fit:1024/1*aDmhHAOamKqEgZJpIIshYg.jpeg"
@@ -84,6 +99,13 @@ const ProductsCard = () => {
             </CardContent>
           </Card>
         ))}
+      </div>
+      <div className="col-span-5 flex justify-center mb-10">
+        <PaginationComponent
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(p) => setCurrentPage(p)}
+        />
       </div>
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '@/components/home/banner';
 import ProductsCard from '@/components/home/ProductsCard';
+import Footer from '@/components/home/Footer';
 
 const Home = () => {
   return (
@@ -10,6 +11,9 @@ const Home = () => {
       </section>
       <section className="ubuntu-font">
         <ProductsCard />
+      </section>
+      <section className="bg-gray-100 ubuntu-font p-8">
+        <Footer />
       </section>
     </div>
   );
