f40517232c5b73eedb157cbef5e0c4d16b491b23
egipsinargo <egipsinargo123@gmail.com>
2025-08-18 23:40:10 +0700

    feat: add some new field for user profile

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,60 +1,72 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: function () {
-      return !this.provider;
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: function () {
+        return !this.provider;
+      },
+    },
+    provider: {
+      type: String,
+      enum: ["google", "github"],
+      default: null,
+    },
+    providerId: {
+      type: String,
+      default: null,
+    },
+    avatarUrl: {
+      type: String,
+      default: null,
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    resetOtp: {
+      type: String,
+      default: null,
+    },
+    resetOtpExpires: {
+      type: Date,
+      default: null,
+    },
+    addresses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
+    deletedAt: {
+      type: Date,
+      default: null,
     },
   },
-  provider: {
-    type: String,
-    enum: ["google", "github"],
-    default: null,
-  },
-  providerId: {
-    type: String,
-    default: null,
-  },
-  avatarUrl: {
-    type: String,
-    default: null,
-  },
-  role: {
-    type: String,
-    default: "user",
-    enum: ["user", "admin"],
-  },
-  resetOtp: {
-    type: String,
-    default: null,
-  },
-  resetOtpExpires: {
-    type: Date,
-    default: null,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: null,
-  },
-  deletedAt: {
-    type: Date,
-    default: null,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
