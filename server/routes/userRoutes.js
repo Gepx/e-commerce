38b0b34e15727c82da058ce2a14a214c3f766beb
egipsinargo <egipsinargo123@gmail.com>
2025-08-18 23:42:33 +0700

    feat: add role validation

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,32 @@
 const express = require("express");
-const { verifyToken } = require("../middleware/authMiddleware");
+const verifyToken = require("../middleware/authMiddleware");
 const UserController = require("../controllers/userController");
+const authorizeRoles = require("../middleware/roleMiddleware");
 const router = express.Router();
 
-router.get("/", UserController.getAllUsersController);
-router.get("/:email", verifyToken, UserController.getUserByEmailController);
-router.put("/:id", verifyToken, UserController.updateUserController);
-router.delete("/:id", verifyToken, UserController.deleteUserController);
+router.get(
+  "/",
+  verifyToken,
+  authorizeRoles("admin"),
+  UserController.getAllUsersController
+);
+router.get(
+  "/:email",
+  verifyToken,
+  authorizeRoles("admin"),
+  UserController.getUserByEmailController
+);
+router.put(
+  "/:id",
+  verifyToken,
+  authorizeRoles("admin"),
+  UserController.updateUserController
+);
+router.delete(
+  "/:id",
+  verifyToken,
+  authorizeRoles("admin"),
+  UserController.deleteUserController
+);
 
 module.exports = router;
