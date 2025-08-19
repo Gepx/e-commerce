d4d6fe2c7b4baa6e155e61f5b196b2c8dfa0ad77
egipsinargo <egipsinargo123@gmail.com>
2025-08-20 00:02:49 +0700

    feat: setup cloudinary and multer for image upload

---
+++ server/config/cloudinary.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+const cloudinary = cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const streamUpload = (fileBuffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        upload_preset: "e-commerce",
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+      }
+    );
+    streamifier.createReadStream(fileBuffer).pipe(stream);
+  });
+};
+
+module.exports = streamUpload;
