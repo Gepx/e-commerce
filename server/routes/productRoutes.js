d8d51abbc3422ed181d517be99ba45561795caac
egipsinargo <egipsinargo123@gmail.com>
2025-08-23 23:14:00 +0700

    feat: add images upload

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const ProductController = require("../controllers/productController");
+const upload = require("../config/multer");
 const router = express.Router();
 
 router.get("/", ProductController.getAllProducts);
 router.get("/:id", ProductController.getProductById);
-router.post("/", ProductController.createProduct);
-router.put("/:id", ProductController.updateProduct);
+router.post(
+  "/",
+  upload.array("productImages", 10),
+  ProductController.createProduct
+);
+router.put(
+  "/:id",
+  upload.array("productImages", 10),
+  ProductController.updateProduct
+);
 router.delete("/:id", ProductController.deleteProduct);
 
 module.exports = router;
