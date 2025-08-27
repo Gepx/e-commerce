5fa2c1c198a49240e937f1d64d3b08c0d0609919
egipsinargo <egipsinargo123@gmail.com>
2025-08-27 22:43:45 +0700

    feat: add maximum request for api

--- server/utils/rateLimiter.js
+++ server/utils/rateLimiter.js
@@ -2,12 +2,12 @@ const rateLimit = require("express-rate-limit");
 
 module.exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 50,
+  max: 1000,
   message: "Too many requests, please try again later.",
 });
 
 module.exports.authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 50,
+  max: 10,
   message: "Too many authentication attempts, please try again later.",
 });
