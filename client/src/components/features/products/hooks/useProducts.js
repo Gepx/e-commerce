11badf02b1e8cf0b6ec439c6344281f9c6921d28
egipsinargo <egipsinargo123@gmail.com>
2025-09-17 09:19:53 +0700

    feat: input search item feature

--- client/src/components/features/products/hooks/useProducts.js
+++ client/src/components/features/products/hooks/useProducts.js
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import productService from '@/components/features/products/services/productService';
+import { useSearchParams } from 'react-router-dom';
 
 const useProducts = () => {
+  const [params] = useSearchParams();
+  const search = params.get('search') || undefined;
+
   return useQuery({
-    queryKey: ['products'],
-    queryFn: () => productService.getAllProducts()
+    queryKey: ['products', { search }],
+    queryFn: () => productService.getAllProducts({ search })
   });
 };
 
