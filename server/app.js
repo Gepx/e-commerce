b88ccc00fa10b34225fac6ba2f42487234ed7ef3
egipsinargo <egipsinargo123@gmail.com>
2025-09-24 23:19:43 +0700

    fix zod

--- server/app.js
+++ server/app.js
@@ -21,6 +21,8 @@ import { limiter } from "./utils/rateLimiter.js";
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
