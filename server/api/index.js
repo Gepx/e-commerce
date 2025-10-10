8cfe6ad4d7b1866ac0f2aeeb41a44cb29008ef1a
egipsinargo <egipsinargo123@gmail.com>
2025-10-10 15:51:12 +0700

    test again 3

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
-import serverless from "serverless-http";
 import app from "../app.js";
 
-export const handler = serverless(app);
-export default handler;
+export default function handler(req, res) {
+  return app(req, res);
+}
